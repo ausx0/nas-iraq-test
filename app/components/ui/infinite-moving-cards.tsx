18d965dae85bf0028e80fd3a3ac3cf0483d0f89c
ausx0 <ahmedalnaemy112233@gmail.com>
2024-10-12 23:01:32 +0300

    chore: add animations reusable components

---
+++ app/components/ui/infinite-moving-cards.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { cn } from "@/app/lib/utils";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+export const InfiniteMovingCards = ({
+  items,
+  direction = "left",
+  speed = "fast",
+  className,
+}: {
+  items: {
+    image: string;
+  }[];
+  direction?: "left" | "right";
+  speed?: "fast" | "normal" | "slow";
+  pauseOnHover?: boolean;
+  className?: string;
+}) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "80s");
+      }
+    }
+  };
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20  max-w-7xl overflow-hidden  ",
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          " flex min-w-full shrink-0 gap-1 py-4 w-max flex-nowrap",
+          start && "animate-scroll "
+        )}
+      >
+        {items.map((item) => (
+          <li
+            className="w-[350px] max-w-full justify-center items-center flex relative rounded-md border border-b-0 flex-shrink-0 px-8 py-2 md:w-[450px]"
+            style={{
+              background: "white",
+            }}
+            key={item.image}
+          >
+            <Image src={`/${item.image}`} width={50} height={50} alt="image" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
