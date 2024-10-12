bc00071ceffcfdb93270b9c6d2ec5574bb6f2bbe
ausx0 <ahmedalnaemy112233@gmail.com>
2024-10-12 23:02:47 +0300

    chore: add iconis and images

---
+++ public/icons/burgerIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BurgerIcon = () => {
+  return (
+    <svg
+      width="23"
+      height="21"
+      viewBox="0 0 23 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line y1="1" x2="23" y2="1" stroke="white" strokeWidth="2" />
+      <line y1="10.2" x2="23" y2="10.2" stroke="white" strokeWidth="2" />
+      <line y1="19.4" x2="23" y2="19.4" stroke="white" strokeWidth="2" />
+    </svg>
+  );
+};
+
+export default BurgerIcon;
