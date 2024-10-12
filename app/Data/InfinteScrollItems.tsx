50a02a16d59852328904c40134f9a8f8716e5121
ausx0 <ahmedalnaemy112233@gmail.com>
2024-10-12 23:01:50 +0300

    update: add fake data

---
+++ app/Data/InfinteScrollItems.tsx
@@ -0,0 +1,11 @@
+export const InfinteScrollItems = [
+  {
+    image: "huawei.png",
+  },
+  {
+    image: "bmw.png",
+  },
+  {
+    image: "nasa.png",
+  },
+];
---
+++ app/Data/NavbarItems.tsx
@@ -0,0 +1,9 @@
+const jobCount = 12;
+
+export const menuItems = [
+  { label: "Home" },
+  { label: "About us" },
+  { label: "Services" },
+  { label: "Branches" },
+  { label: `Jobs (${jobCount})` },
+];
